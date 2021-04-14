7909670bef13dd9ba607b41a38977d51427f56c9
Mateus Rodrigues Silva <73179530+Mateusrodrigues75@users.noreply.github.com>
2021-04-14 09:18:17 -0300

    api corrigida

--- M_Peoples/Senai.Peoples.WebApi/Senai.Peoples.WebApi/Domain/FuncionariosDomain.cs
+++ M_Peoples/Senai.Peoples.WebApi/Senai.Peoples.WebApi/Domain/FuncionariosDomain.cs
@@ -13,7 +13,10 @@ namespace Senai.Peoples.WebApi.Domain
 
         [Required(ErrorMessage = "O sobrenome do funcionário é obrigatório!")]
         [StringLength(10, MinimumLength = 5, ErrorMessage = "O sobrenome do funcionário deve conter de 5 a 10 caracteres")]
+        //[RegularExpression()] - Para controlar tipos de caracteres.
         public string sobrenome { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime DataNascimento { get; set; }
     }
 }
