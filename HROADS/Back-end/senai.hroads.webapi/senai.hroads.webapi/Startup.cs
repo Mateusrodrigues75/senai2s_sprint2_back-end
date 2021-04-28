73a7e3087e282b8e414bc6fea8e2d628f0cc264c
Mateus Rodrigues Silva <73179530+Mateusrodrigues75@users.noreply.github.com>
2021-04-27 22:31:13 -0300

    HROADS - Swagger add

--- HROADS/Back-end/senai.hroads.webapi/senai.hroads.webapi/Startup.cs
+++ HROADS/Back-end/senai.hroads.webapi/senai.hroads.webapi/Startup.cs
@@ -4,9 +4,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace senai.hroads.webapi
@@ -18,6 +21,18 @@ namespace senai.hroads.webapi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "hroads.webApi", Version = "v1" });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
             services
                 .AddAuthentication(options =>
                 {
@@ -56,6 +71,16 @@ namespace senai.hroads.webapi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "hroads.webApi");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
